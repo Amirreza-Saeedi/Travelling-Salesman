e0925471d88d50fd6aeb47f46ed4e9386974d1b8
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-04-11 22:58:44 +0430

    add Trace class

---
+++ src/Trace.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public class Trace extends Rectangle {
+    int _x; // board x coordinate
+    int _y; // board y coordinate
+
+    public Trace(int x, int y, int width, int height) {
+        super(x, y, width, height);
+    }
+
+    public Trace(Dimension d) {
+        super(d);
+    }
+
+    public void setTrace(int xPixel, int yPixel, int xCoordinate, int yCoordinate) {
+        this.x = xPixel;
+        this.y = yPixel;
+        this._x = xCoordinate;
+        this._y = yCoordinate;
+    }
+
+    public void draw(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setColor(new Color(0x81000000, true));
+        g2.fillRect(x, y, width, height);
+    }
+}

d1061020908de929929d79eeddd0215422a7dc8a
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-04-12 14:29:43 +0430

    add fight method

--- src/element/Player.java
+++ src/element/Player.java
@@ -3,10 +3,12 @@ package element;
 import java.awt.*;
 
 public class Player extends Element {
-    // TODO players pieces
+    private static final int INITIAL_POWER = 50;
+    private static final int INITIAL_CASH = 100;
     private int[] lootedTreasures; // saves treasure ID
 
     private int power;
+    private int coin;
 
     private static Color[] colors = {
             new Color(0xE00000),
@@ -17,11 +19,13 @@ public class Player extends Element {
         super(x, y, width, height);
         this.setColor(colors[id]);
         this.setTitle(title);
-
         this.newLabel(getTitle()); // create label
+
+        this.power = INITIAL_POWER;
+        this.coin = INITIAL_CASH;
     }
 
-    public void draw(Graphics g) { // draws a bordered-filled rectangle & it's image within
+    public void draw(Graphics g) { // draws a bordered-filled circle & it's image within
         Graphics2D g2 = (Graphics2D) g;
         g2.setStroke(new BasicStroke(1));
 
@@ -39,5 +43,36 @@ public class Player extends Element {
         heightImg = this.height - 2 * this.height / 10;
 
         g2.drawImage(this.image, xImg, yImg, widthImg, heightImg, null);
+    }
+
+    public void fight(Player opponent, StartHouse startHouse) {
+        System.out.println("Player.fight");
+        Player winner, looser;
+        if (this.power >= opponent.power) { // declare winner
+            winner = this;
+            looser = opponent;
+        } else {
+            winner = opponent;
+            looser = this;
+        }
+        System.out.println("winner = " + winner);
+        System.out.println("looser = " + looser);
+
+        // coin changes
+        int coin = (int) (((double) winner.power - looser.power) / (winner.power + looser.power) * looser.coin);
+        System.out.println("coin = " + coin);
+        winner.coin += coin;
+        looser.coin -= coin;
+
+        // power changes
+        winner.power -= looser.power;
+        looser.power = 0;
+        System.out.println("winner.power = " + winner.power);
+
+        // revive the looser
+        looser.x = startHouse.x;
+        looser.y = startHouse.y;
+        looser._x = startHouse._x;
+        looser._y = startHouse._y;
     }
 }

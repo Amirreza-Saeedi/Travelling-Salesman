ba52cf5275c04a7530689be0cc482d3bab35574d
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-04-16 23:51:17 +0430

    add classifier

--- src/element/Trap.java
+++ src/element/Trap.java
@@ -1,13 +1,49 @@
 package element;
 
 import java.awt.*;
+import java.util.Random;
 
 public class Trap extends Element {
-    public Trap(int x, int y, int width, int height, int id, int _x, int _y) {
+    protected int physicalDamage;
+    protected int financialDamage;
+    private static final int[] physicalDamages = {5, 10};
+    private static final int[] financialDamages = {25, 50};
+    public Trap(int x, int y, int width, int height, int id, int _x, int _y, int physicalDamage, int financialDamage) {
         super(x, y, width, height, id, _x, _y, "Trap");
         this.setColor(new Color(0xFF0202));
         newLabel(getTitle()); // create label
 
         setVisible(false);
+    }
+      public Trap(int x, int y, int width, int height, int id, int _x, int _y) {
+        this(x, y, width, height, id, _x, _y, 0, 0);
+    }
+
+    public static int createPhysicalDamage() {
+        return physicalDamages[new Random().nextInt(2)];
+    }
+
+    public static int createFinancialDamage() {
+        return financialDamages[new Random().nextInt(2)];
+    }
+
+    public void classify() { // puts trap in one of the 4 kinds
+        if (physicalDamage == physicalDamages[1] && financialDamage == financialDamages[1]) {
+            this.title = "Malicious Trap";
+        } else if (physicalDamage == physicalDamages[1] && financialDamage == financialDamages[0]) {
+            this.title = "Deadly Trap";
+        } else if (physicalDamage == physicalDamages[0] && financialDamage == financialDamages[1]) {
+            this.title = "Greedy Trap";
+        } else if (physicalDamage == physicalDamages[0] && financialDamage == financialDamages[0]) {
+            this.title = "Minor Trap";
+        }
+    }
+
+    public int getFinancialDamage() {
+        return financialDamage;
+    }
+
+    public int getPhysicalDamage() {
+        return physicalDamage;
     }
 }

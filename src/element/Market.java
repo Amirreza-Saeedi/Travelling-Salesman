7cd0b04a8bd2829c5749229b86d13f504646327a
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-05-01 18:49:19 +0430

    clear Market

--- src/element/Market.java
+++ src/element/Market.java
@@ -1,25 +1,16 @@
 package element;
 
-
-import javax.swing.*;
-import javax.swing.border.EtchedBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 public class Market extends Element {
-
+    public final static Color COLOR = new Color(0xFFC002);
     public Market(int x, int y, int width, int height, int id, int _x, int _y) {
         super(x, y, width, height, id, _x, _y, "Market");
-        this.setColor(new Color(0xFFC002));
+        this.setColor(COLOR);
         newLabel(getTitle()); // create label
 
         setVisible(true);
     }
-
-
 }
 
 

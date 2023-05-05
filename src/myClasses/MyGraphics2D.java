65079646f22aadbcf8a139373177a019e1dfc688
Amirreza-Saeedi <amirrezasaeedi3@gmail.com>
2023-05-05 14:20:39 +0430

    update package organization

--- src/myClasses/MyGraphics2D.java
+++
@@ -1,9 +0,0 @@
-package myClasses;
-import java.awt.*;
-
-public abstract class MyGraphics2D extends Graphics2D {
-
-    public void fillRect(Rectangle rectangle) {
-        fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-    }
-}
